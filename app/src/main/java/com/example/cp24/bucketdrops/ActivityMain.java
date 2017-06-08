956bf2f52bbc851e5b8df0ab85f8b6abf757e328
K. Goscinski <krzysztof.goscinski@gmail.com>
2017-06-08 20:58:22 +0200

    Lesson 47/48 - Adding a toolbar XML /  Display Bitmaps with Glide

--- app/src/main/java/com/example/cp24/bucketdrops/ActivityMain.java
+++ app/src/main/java/com/example/cp24/bucketdrops/ActivityMain.java
@@ -2,10 +2,15 @@ package com.example.cp24.bucketdrops;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+
 
 public class ActivityMain extends AppCompatActivity {
     ImageView logo;
+    Toolbar mToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +21,18 @@ public class ActivityMain extends AppCompatActivity {
         matrix.setRotate(10.0F);
         logo.setImageMatrix(matrix);*/
 
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
+        initBackgroundImage();
 
+
+    }
+
+    private void initBackgroundImage() {
+        ImageView background = (ImageView) findViewById(R.id.iv_background);
+        Glide.with(this)
+                .load(R.drawable.background)
+                .centerCrop()
+                .into(background);
     }
 }
