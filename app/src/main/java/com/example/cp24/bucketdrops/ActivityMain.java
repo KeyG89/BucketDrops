48efce7577a11200a3d32d53d756ff0b8b95b3be
K. Goscinski <krzysztof.goscinski@gmail.com>
2017-06-11 16:39:28 +0200

    Lesson 58 - Setup Dialog Add in Java

--- app/src/main/java/com/example/cp24/bucketdrops/ActivityMain.java
+++ app/src/main/java/com/example/cp24/bucketdrops/ActivityMain.java
@@ -3,6 +3,8 @@ package com.example.cp24.bucketdrops;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
@@ -11,6 +13,20 @@ import com.bumptech.glide.Glide;
 public class ActivityMain extends AppCompatActivity {
     ImageView logo;
     Toolbar mToolbar;
+    Button mBtnAdd;
+
+    private View.OnClickListener mBtnAddListener = new View.OnClickListener() {
+
+        @Override
+        public void onClick(View v) {
+            showDialogAdd();
+        }
+    };
+
+    private void showDialogAdd() {
+        DialogAdd dialog = new DialogAdd();
+        dialog.show(getSupportFragmentManager(), "Add");
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +36,9 @@ public class ActivityMain extends AppCompatActivity {
 /*        Matrix matrix = new Matrix();
         matrix.setRotate(10.0F);
         logo.setImageMatrix(matrix);*/
+
+        mBtnAdd = (Button) findViewById(R.id.btn_add);
+        mBtnAdd.setOnClickListener(mBtnAddListener);
 
         mToolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(mToolbar);
